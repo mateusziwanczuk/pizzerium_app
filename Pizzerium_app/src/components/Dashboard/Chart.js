3c1740f38d906e7de4b527715ad830f1caa04bb0
mateuszmatzh <iwanczukmateusz@gmail.com>
2019-05-29 16:21:46 +0200

    Dashboard: RWD completed. Fix charts sizes (responsive).

--- Pizzerium_app/src/components/Dashboard/Chart.js
+++ Pizzerium_app/src/components/Dashboard/Chart.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { db } from "../../App";
 
+import './dashboard.css'
+
 const BarChart = require("react-chartjs").Bar;
 
 class Chart1 extends React.Component {
@@ -83,7 +85,14 @@ class Chart1 extends React.Component {
         }
       ]
     };
-    return <BarChart className="chart" data={chartData} height={220} width={480} />;
+    const option = {
+      responsive: true
+    };
+    return <BarChart 
+      className="chart" 
+      data={chartData}
+      options={option}
+    />;
   }
 }
 
